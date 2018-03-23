510910632d476153249b3d663cd03480ec236df5
frankkfang <374547123@qq.com>
2018-03-23 14:50:36 +0800

    git branch --set-upstream master origin/master

--- src/NihaoShijie.java
+++ src/NihaoShijie.java
@@ -2,5 +2,6 @@ public class NihaoShijie {
     public static void main(String[] args){
         System.out.print("你好世界！");
 
+        System.out.print("你好世界！");
     }
 }
---
+++ src/heheda.java
@@ -0,0 +1,6 @@
+public class heheda {
+    public static void main(String[] args){
+        System.out.print("你好ya！");
+
+    }
+}
